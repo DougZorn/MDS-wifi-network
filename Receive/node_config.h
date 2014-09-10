7bcef379788d3841961645c93ab5cb9fd206ec2b
DougHomeWin7 <namelessone.doug@gmail.com>
2014-09-10 12:28:24 -0700

    Made node_config.h more configurable

--- Receive/node_config.h
+++ Receive/node_config.h
@@ -1,11 +1,31 @@
 #ifndef NODE_CONFIG_H
 #define NODE_CONFIG_H
 
-#define NODE_ID_ADDRESS 0x01
-#define NEXT_NODE_ADDRESS 0x02
-#define INITIAL_STATE 0x01
 #define TX_TIMEOUT 100 // in milliseconds
+#define LAST_NODE 0x09 
 
-#define LAST_NODE 0x09
+#define NODE 0x01 //set for each node. Settings for each are below
+
+#if NODE == 0x01
+	#define NODE_ID_ADDRESS 0x01
+	#define NEXT_NODE_ADDRESS 0x02
+	#define INITIAL_STATE 0x01
+#elif NODE == 0x02
+	#define NODE_ID_ADDRESS 0x02
+	#define NEXT_NODE_ADDRESS 0x03
+	#define INITIAL_STATE 0x01
+#elif NODE == 0x03
+	#define NODE_ID_ADDRESS 0x03
+	#define NEXT_NODE_ADDRESS 0x04
+	#define INITIAL_STATE 0x01
+#elif NODE == 0x04
+	#define NODE_ID_ADDRESS 0x04
+	#define NEXT_NODE_ADDRESS 0x05
+	#define INITIAL_STATE 0x01
+#elif NODE == 0x05
+	#define NODE_ID_ADDRESS 0x05
+	#define NEXT_NODE_ADDRESS 0x01
+	#define INITIAL_STATE 0x01
+#endif
 
 #endif
