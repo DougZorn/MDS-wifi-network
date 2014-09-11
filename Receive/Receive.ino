68a747b7c33e98744646526f8de7a33eeb297486
DougHomeWin7 <namelessone.doug@gmail.com>
2014-09-11 10:42:31 -0700

    Working RR

--- Receive/Receive.ino
+++ Receive/Receive.ino
@@ -27,26 +27,32 @@ void setup()
 	Serial.println(INITIAL_STATE, HEX);
 	*/	
 } 
-
+#define ACK_DM 0x08
 #define Bpacket 0x07
 #define NB1packet 0x06
-#define NB2packet 0x06
 #define NTDMpacket 0x05
 
 //length (not including self), address, type, Self ID, packet type. The length byte shows up on RX FIFO
+byte ACKDMPACKET[] = {0x05,0xFF,0x01,NODE_ID_ADDRESS,ACK_DM,ACK_DM}; //bytes on RXFIFO: 8
 byte BroadcastPacket[] = {0x04,0xFF,0x01,NODE_ID_ADDRESS,Bpacket}; //bytes on RXFIFO: 7
-byte HandShakeNextBroadcast[] = {0x03,NEXT_NODE_ADDRESS,NB1packet,NB2packet}; //bytes on RXFIFO: 6
+byte HandShakeNextBroadcast[] = {0x03,NEXT_NODE_ADDRESS,NB1packet,NB1packet}; //bytes on RXFIFO: 6
 byte HandShakeNextTransmitDM[] = {0x02,NEXT_NODE_ADDRESS,NTDMpacket}; //bytes on RXFIFO: 5
 
 //states FIX NUMBERS !!!! DUPLICATES
 #define SEND_HANDSHAKE_PACKET_B 0x01
 #define SEND_HANDSHAKE_PACKET_DM 0x02
-#define WAIT_4_ACK 0x03
+#define WAIT_4_ACK_B 0x03
 #define LISTEN_4_PACKET 0x04
 #define BROADCAST 0x05
+#define WAIT_4_ACK_DM 0x06
+#define SEND_ACK_DM 0x07// did not implement
 
 byte state = INITIAL_STATE;
 //byte packet[] = {0x01, 0xFF};
+
+byte Dmatirx[LAST_NODE] = {0};
+byte NodeID = 0;
+byte RSSI = 0;
 
 void loop()
 {
@@ -56,12 +62,19 @@ void loop()
 			Serial.println("Entered LISTEN_4_PACKET STATE");
 			if(listenForPacket(&myQueue1))	//edit return type to be one of the three posssible values
 			{
-				Serial.println("packet Received...");
-				//Serial.print("myQueue count: ");
-                //Serial.println(myQueue1.count(),HEX);				
+				Serial.println("packet Received...");							
 				if(Bpacket == myQueue1.count()) //expect peek to be 1 less then count. peek is the lenght which does not include itself
 				{
+					NodeID = 0;
 					Serial.println("Storing data...");
+					myQueue1.pop();
+					myQueue1.pop();
+					myQueue1.pop();
+					NodeID = myQueue1.pop()-1;
+					myQueue1.pop();
+					RSSI = myQueue1.pop();
+					myQueue1.pop();
+					Dmatirx[NodeID] = convert_dBm(RSSI);					
 					state = LISTEN_4_PACKET; //you need to write code to store the data
 				}
 				else if(NB1packet == myQueue1.count())
@@ -69,10 +82,11 @@ void loop()
 					Serial.println("Broadcast HandShake Recieved...");
 					state = BROADCAST;
 				}
-				else //5
+				else if(NTDMpacket == myQueue1.count())//5
 				{
 					Serial.println("DisM HandShake Received");
 					Serial.println("Sending Dis Matrix...");
+					sendFixedPacket(ACKDMPACKET,6);			
 					//insert code to transmit dis matrix to processing node
 					if (LAST_NODE == NODE)
 					{			   
@@ -83,14 +97,22 @@ void loop()
 						state = SEND_HANDSHAKE_PACKET_DM;
 					}
 				
-				}				
+				}
+				else
+				{
+					state = LISTEN_4_PACKET;
+				}
 				EmptyQueueList(&myQueue1); //this is just to test state transitions	
 			}
 			break;
+			
+		case SEND_ACK_DM:
+			
+		
+			break;
 		case BROADCAST:
 			Serial.println("Entered BROADCAST STATE");
-			digitalWrite(9, HIGH);
-			//delay(1000);
+			digitalWrite(9, HIGH);			
 			sendFixedPacket(BroadcastPacket,5);			
 			if (LAST_NODE == NODE)
 			{
@@ -104,22 +126,38 @@ void loop()
 			break;
 		case SEND_HANDSHAKE_PACKET_B:
 			Serial.println("Entered SEND_HANDSHAKE_B STATE");
+			Serial.print("Sent to: ");
+			Serial.println(NEXT_NODE_ADDRESS,HEX);
 			sendFixedPacket(HandShakeNextBroadcast,4);						
-			state = WAIT_4_ACK;
+			state = WAIT_4_ACK_B;
 			break;
 		case SEND_HANDSHAKE_PACKET_DM:                  // this currently does not have an ACK when transmitting the DM
-			Serial.println("Entered SEND_HANDSHAKE_PACKET_DM STATE");			
+			Serial.println("Entered SEND_HANDSHAKE_PACKET_DM STATE");
+			Serial.print("Sent to: ");
+			Serial.println(NEXT_NODE_ADDRESS,HEX);
 			sendFixedPacket(HandShakeNextTransmitDM,3);
 			digitalWrite(9, LOW);
-			state = LISTEN_4_PACKET;
+			state = WAIT_4_ACK_DM;
 			break;
-		case WAIT_4_ACK:
-			Serial.println("Entered WAIT_4_ACK STATE");
+		case WAIT_4_ACK_DM:
+			Serial.println("Entered WAIT_4_ACK_DM STATE");
 			if(listenForPacket(&myQueue2)) //listenForPacket is blocking function that has timeout built in as return value 0
 			{				
-				Serial.println("ACK received");
-				//Serial.print("myQueue2 count: ");
-                //Serial.println(myQueue2.count(),HEX);
+				Serial.println("ACK_DM received");				
+				state = LISTEN_4_PACKET;
+				digitalWrite(9, LOW);
+				EmptyQueueList(&myQueue2); //this is just to test state transitions	
+			}
+			else
+			{
+				state = SEND_HANDSHAKE_PACKET_DM;
+			}			
+			break;			
+		case WAIT_4_ACK_B:
+			Serial.println("Entered WAIT_4_ACK_B STATE");
+			if(listenForPacket(&myQueue2)) //listenForPacket is blocking function that has timeout built in as return value 0
+			{				
+				Serial.println("ACK_B received");				
 				state = LISTEN_4_PACKET;
 				digitalWrite(9, LOW);
 				EmptyQueueList(&myQueue2); //this is just to test state transitions	
--- Receive/node_config.h
+++ Receive/node_config.h
@@ -1,10 +1,10 @@
 #ifndef NODE_CONFIG_H
 #define NODE_CONFIG_H
 
-#define TX_TIMEOUT 100 // in milliseconds
-#define LAST_NODE 0x02 
+#define TX_TIMEOUT 150 // in milliseconds
+#define LAST_NODE 0x05 
 
-#define NODE 0x02 //set for each node. Settings for each are below
+#define NODE 0x05 //set for each node. Settings for each are below
 
 //when defining NEXT_NODE ADDRESS
 // states
@@ -13,27 +13,28 @@
 // WAIT_4_ACK 0x03
 // LISTEN_4_PACKET 0x04
 // BROADCAST 0x05
+// WAIT_4_ACK_DM 0x06
 
 #if NODE == 0x01
 	#define NODE_ID_ADDRESS 0x01
 	#define NEXT_NODE_ADDRESS 0x02
-	#define INITIAL_STATE 0x05
+	#define INITIAL_STATE 0x04
 #elif NODE == 0x02
 	#define NODE_ID_ADDRESS 0x02
-	#define NEXT_NODE_ADDRESS 0x01
+	#define NEXT_NODE_ADDRESS 0x03
 	#define INITIAL_STATE 0x04
 #elif NODE == 0x03
 	#define NODE_ID_ADDRESS 0x03
 	#define NEXT_NODE_ADDRESS 0x04
-	#define INITIAL_STATE 0x01
+	#define INITIAL_STATE 0x04
 #elif NODE == 0x04
 	#define NODE_ID_ADDRESS 0x04
 	#define NEXT_NODE_ADDRESS 0x05
-	#define INITIAL_STATE 0x01
+	#define INITIAL_STATE 0x04
 #elif NODE == 0x05
 	#define NODE_ID_ADDRESS 0x05
 	#define NEXT_NODE_ADDRESS 0x01
-	#define INITIAL_STATE 0x01
+	#define INITIAL_STATE 0x05
 #endif
 
 #endif
