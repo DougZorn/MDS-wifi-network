e6c205cb946f0815104f2183433a60c7cf590f15
DougHomeWin7 <namelessone.doug@gmail.com>
2014-09-22 16:08:30 -0700

    Added 2 node serial file print

--- SerialPython/serialmonitor.py
+++ SerialPython/serialmonitor.py
@@ -1,28 +1,15 @@
 import serial
 ser = serial.Serial(2)
 print ser.name
-line1 = ser.readline()
-line2 = ser.readline()
-line3 = ser.readline()
-line4 = ser.readline()
-line5 = ser.readline()
-line6 = ser.readline()
-line7 = ser.readline()
-line8 = ser.readline()
-line9 = ser.readline()
-line10 = ser.readline()
-line11 = ser.readline()
-
-
-import serial
-ser = serial.Serial(2)
-print ser.name
 f = open('testwrite.csv' , 'w')
-f.write(ser.readline())
-f.write(ser.readline())
-f.write(ser.readline())
-f.write(ser.readline())
-f.write(ser.readline())
+while ser.read() != '1':
+	pass
 
+f.write('1')
+f.write(ser.readline())
+while ser.read() != '2':
+	pass
 
+f.write('2')
+f.write(ser.readline())
 f.close()
