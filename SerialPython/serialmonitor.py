cdecde613404dd2342220b0ec705a08357dc914b
DougHomeWin7 <namelessone.doug@gmail.com>
2014-09-23 09:37:54 -0700

    Added for loop instead of hardcode

--- SerialPython/serialmonitor.py
+++ SerialPython/serialmonitor.py
@@ -4,28 +4,12 @@ print ser.name
 ser.flushInput()
 f = open('testwrite.csv' , 'w')
 
-while ser.read() != '1':
-	pass
-
-f.write('1')
-f.write(ser.readline())
-
-while ser.read() != '2':
-	pass
-
-f.write('2')
-f.write(ser.readline())
-
-while ser.read() != '3':
-	pass
-
-f.write('3')
-f.write(ser.readline())
-
-while ser.read() != '4':
-	pass
-
-f.write('4')
-f.write(ser.readline())
+#loop runs from 1 to n-1. so if you pass a param for the number of nodes be sure to add one to the loop
+for x in range(1,5):
+	while ser.read() != str(x):
+		pass
+	
+	f.write(str(x))
+	f.write(ser.readline())
 
 f.close()
